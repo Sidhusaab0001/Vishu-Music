9c785a5c4695ba5163290109bc61953119ad87e9
ɴᴏᴏʙ sɪᴅʜᴜ <132650820+Sidhusaab0001@users.noreply.github.com>
2024-03-12 12:45:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Vishu-Music/ShizuMusic",
+    "https://github.com/Sidhusaab0001/Vishu-music",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
